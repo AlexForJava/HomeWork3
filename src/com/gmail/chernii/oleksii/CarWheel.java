5ff52ecd2f04143abf98c4c9bf2e16fae667b546
AlexForJava <hqq@ukr.net>
2019-03-01 00:34:19 +0200

    carwheel branch first commit

---
+++ src/com/gmail/chernii/oleksii/CarWheel.java
@@ -0,0 +1,17 @@
+package com.gmail.chernii.oleksii;
+
+/**
+ * Created by Space on 28.02.2019.
+ */
+public class CarWheel {
+    private double state;
+
+    public CarWheel() {
+        this.state = Math.random();
+    }
+
+    public CarWheel(double state) {
+        this.state = state;
+    }
+
+}
