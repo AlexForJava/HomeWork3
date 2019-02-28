9eeb46f12b4224a0247de9be37157a1cebaa0898
AlexForJava <hqq@ukr.net>
2019-03-01 00:30:13 +0200

    first commit

---
+++ src/com/gmail/chernii/oleksii/CarDoor.java
@@ -0,0 +1,19 @@
+package com.gmail.chernii.oleksii;
+
+/**
+ * Created by Space on 28.02.2019.
+ */
+public class CarDoor {
+    private boolean doorOpen;
+    private boolean windowOpen;
+
+    public CarDoor() {
+        this.doorOpen = false;
+        this.doorOpen = false;
+    }
+
+    public CarDoor(boolean doorOpen, boolean windowOpen) {
+        this.doorOpen = doorOpen;
+        this.windowOpen = windowOpen;
+    }
+}
